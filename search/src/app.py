74a4cc88e922aab6cd732002d19fb8ddfac1c78e
Vigneshwar Padmanaban <swift2891@gmail.com>
2017-10-13 12:58:48 -0400

    Updated the Search Module, with a complete functionality to search people in UI and get a list of people in UI

--- search/src/app.py
+++ search/src/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify, request, render_template, session
-
 from common.database import Database
+from find_people import Find_People
+
+#Flask Section
 
 app = Flask(__name__)
 
@@ -12,15 +14,25 @@ def home():
 @app.route('/search/skill', methods=['POST'])
 def login_user():
     Skill = request.form['Skill']
-    print(Skill)
+    s = "Skill being requested: {}"
+    print(s.format(Skill))
+
+    #Searching people in MongoDB for skill = Skill:
+    results = Find_People.search_from_mongo(skill=Skill)
+    print("List of people with requested skill:")
+    peopleNames=[]
+    for people in results:
+        peopleNames.append(people['Name'])
+        print(people['Name'])
+
     # return render_template("search2.html", Skill=session['Skill'])
-    return render_template("search2.html")
+    # return render_template("search2.html")
+    return render_template("search2.html", results=peopleNames)
 
 
 @app.before_first_request
 def initialize_database():
     Database.initialize()
-
 
 if __name__ == '__main__':
     app.run(port=4995, debug=True)
