e457dc46d4988ce9b9f4d181240857055332c79d
Vigneshwar Padmanaban <swift2891@gmail.com>
2017-10-14 11:35:40 -0400

    Updating Unittest skeletons for backend

---
+++ Integrated App v2/src/unittest/test.py
@@ -0,0 +1,49 @@
+import unittest
+import app
+import find_people
+import user
+
+class TestApp(unittest.TestCase):
+
+    def test_databaseInsert(self):
+        pass
+
+    def test_database_find(self):
+        pass
+
+    def test_database_find_one(self):
+        pass
+
+    def test_find_people(self):
+        pass
+
+    def test_validateUser(self):
+        pass
+
+    def test_getName(self):
+        pass
+
+    def test_index(self):
+        pass
+
+    def test_register(self):
+        pass
+
+    def test_login(self):
+        pass
+
+    def test_regsuccess(self):
+        pass
+
+    def test_userhome(self):
+        pass
+
+    def test_search(self):
+        pass
+
+    def test_addskill(self):
+        pass
+
+    def test_(self):
+        pass
+
