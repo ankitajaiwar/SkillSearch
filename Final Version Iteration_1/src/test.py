c9e6082ee5ba2398df2ff377963869a30e46bf55
Vigneshwar Padmanaban <swift2891@gmail.com>
2017-10-14 22:05:18 -0400

    Fixed a database bug in test.py

--- Final Version Iteration_1/src/test.py
+++ Final Version Iteration_1/src/test.py
@@ -9,6 +9,10 @@ class TestApp(unittest.TestCase):
 
     def test_databaseInsert(self):
         Database.initialize()
+        pwd = sha256_crypt.encrypt("test")
+        Database.insert(collection="user_profiles", data={"fname": "Test_User_Name1",
+                                                          "uname": "test_User1",
+                                                          "password": pwd})
         Database.insert(collection="test_collection", data={"name": "vignesh", "age": "26"})
         test_data=Database.find_one(collection="test_collection",query={"name":"vignesh"})
         self.assertEqual(test_data['age'],'26')
@@ -19,21 +23,18 @@ class TestApp(unittest.TestCase):
         self.assertEqual(test_data['name'], 'vignesh')
 
     def test_validateUser(self):
-        pwd = sha256_crypt.encrypt("test")
-        Database.insert(collection="user_profiles", data={"fname": "Test_User_Name1",
-                                                          "uname": "test_User1",
-                                                          "password": pwd})
+        status = 'not ok'
         if User.validateUser(user_name='test_User1', pwd='test'):
             status='ok'
         self.assertEqual(status,'ok')
 
     def test_getName(self):
-        Database.initialize()
         fName = User.getName(username="test_User1")
         self.assertEqual(fName,"Test_User_Name1")
 
 
     def test_findPeople(self):
+        testName = None
         Database.insert(collection="skillset", data={"Name": "testuser", "skill": "test_Java"})
         print('hi2')
         results = Find_People.search_from_mongo(skill="test_Java")
@@ -41,11 +42,5 @@ class TestApp(unittest.TestCase):
             testName = r['Name']
         self.assertEqual(testName,'testuser')
 
-
-
     if '__name__' == '__main__':
         unittest.main()
-
-
-
-
